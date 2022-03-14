0d1f8d4ee83b116e4826e892709df3fd542ba2c1
Tobias <tobiasurban@hotmail.com>
2022-03-14 10:11:50 +0100

    Added settere og gettere

--- src/Room.java
+++ src/Room.java
@@ -4,4 +4,49 @@ public class Room {
     private Room south;
     private Room east;
     private Room west;
+
+
+
+    void setRoomName (Room roomName){
+        this.roomName = roomName;
+    }
+
+    Room getRoomName (){
+        return roomName;
+    }
+
+    void setNorth (Room north){
+        this.north = north;
+    }
+
+    Room getNorth (){
+        return north;
+    }
+
+    void setSouth (Room south){
+        this.south = south;
+    }
+
+    Room getSouth (){
+        return south;
+    }
+
+    void setEast (Room east){
+        this.east = east;
+    }
+
+    Room getEast (){
+        return east;
+    }
+
+    void setWest (Room west){
+        this.west = west;
+    }
+
+    Room getWest (){
+        return west;
+    }
+
+
+
 }
